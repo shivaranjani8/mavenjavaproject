853b13329337ae46f05df27ad4f5be3cabf4ba62
aishu <aishu@29gmail.com>
2024-11-19 14:37:12 +0530

    First commit to maven java project

---
+++ src/main/java/mavenjavaproject/javaproject/App.java
@@ -0,0 +1,13 @@
+package mavenjavaproject.javaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "shiva ranjani-22BD1A665H" );
+    }
+}
