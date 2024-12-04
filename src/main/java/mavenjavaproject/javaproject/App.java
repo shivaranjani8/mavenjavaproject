4d0ae0a076635b8d894ac9e501b0519d403b41b6
shivaranjani8 <rathodshivaranjani9@gmail.com>
2024-12-04 11:13:51 +0530

    Update App.java

--- src/main/java/mavenjavaproject/javaproject/App.java
+++ src/main/java/mavenjavaproject/javaproject/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "shiva ranjani" );
         System.out.println( "shiva ranjani-22BD1A665H" );
+        System.out.println( "csm");
     }
 }
